dac7a55f83438f255abdbe6a1c7eaf7a2e917f80
Maksim Isakov <isakovmaksim777@gmail.com>
2026-06-29 22:03:04 +0600

    fix ymap from API some redactions of font in .booking

--- js/main.js
+++ js/main.js
@@ -1,16 +1,40 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+window.addEventListener('DOMContentLoaded', function () {
+  'use strict';
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button--next',
-    prevEl: '.swiper-button--prev',
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button--next',
+      prevEl: '.swiper-button--prev',
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+  });
+
+  ymaps.ready(init);
+  function init() {
+    // Создание карты.
+    let myMap = new ymaps.Map("map", {
+      // Координаты центра карты.
+      // Порядок по умолчанию: «широта, долгота».
+      // Чтобы не определять координаты центра карты вручную,
+      // воспользуйтесь инструментом Определение координат.
+      center: [55.040590, 73.281057],
+      // Уровень масштабирования. Допустимые значения:
+      // от 0 (весь мир) до 19.
+      zoom: 16
+    }, {
+      autoFitToViewport: 'always'
+    });
+  };
 
 });
+
+
