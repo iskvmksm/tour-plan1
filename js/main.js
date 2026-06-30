741ca013657879f9ce05b64354f7065b0be19823
Maksim Isakov <isakovmaksim777@gmail.com>
2026-06-30 23:24:35 +0600

    create: section reviews + slider-2 + fix hotel-slider

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 window.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
-  const swiper = new Swiper('.swiper', {
+  const hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
 
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button--next',
-      prevEl: '.swiper-button--prev',
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
     },
     keyboard: {
       enabled: true,
@@ -35,6 +35,25 @@ window.addEventListener('DOMContentLoaded', function () {
     });
   };
 
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+    watchOverflow: false,
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+  });
 });
 
 
